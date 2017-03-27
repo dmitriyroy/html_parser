192c964fe8718c048c973c37a25744aa4f21c52e
dmitriyroy <dmitriy.roy@ita.biz.ua>
2017-03-27 23:28:55 +0300

    Первый коммит

---
+++ src/HtmlGrabber.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by dmitriyroy on 27.03.17.
+ */
+public class HtmlGrabber {
+    File htmlFile;
+    String outFilePath;
+    public HtmlGrabber() {
+    }
+
+    public HtmlGrabber(File htmlFile) {
+        this.htmlFile = htmlFile;
+    }
+
+    public HtmlGrabber(String outFilePath) {
+        this.outFilePath = outFilePath;
+        this.htmlFile = new File(outFilePath);
+    }
+
+    public File getHtmlFile() {
+        return htmlFile;
+    }
+
+    public void setHtmlFile(File htmlFile) {
+        this.htmlFile = htmlFile;
+    }
+
+    public String getOutFilePath() {
+        return outFilePath;
+    }
+
+    public void setOutFilePath(String outFilePath) {
+        this.outFilePath = outFilePath;
+    }
+
+    public File grabSite(String url, String codepage) throws IOException {
+
+        BufferedWriter outFileWriter;
+        StringBuilder sb = new StringBuilder();
+        URL pageURL = new URL(url);
+        URLConnection uc = pageURL.openConnection();
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(
+                        uc.getInputStream(), codepage));
+        try {
+            String inputLine;
+            while ((inputLine = br.readLine()) != null) {
+                sb.append(inputLine);
+            }
+        } finally {
+            br.close();
+        }
+
+        outFileWriter = new BufferedWriter(new FileWriter(this.htmlFile.getCanonicalPath(), false));
+        outFileWriter.write(sb.toString());
+        outFileWriter.close();
+
+        return this.htmlFile;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by dmitriyroy on 27.03.17.
+ */
+public class Main {
+    public static BufferedWriter logFileWriter;
+    public static void main(String[] args) throws IOException {
+        String outFilePath = "/home/dmitriyroy/Data/HtmlFile.txt";
+        File outFile = new File(outFilePath);
+        // открываем лог-файл. дописываем в конец - параметр true
+//        try {
+//            logFileWriter = new BufferedWriter(new FileWriter(outFilePath, true));
+//            logFileWriter.write("===========-======== start "+new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").format(new Date(System.currentTimeMillis()))+ "\n");
+//            logFileWriter.write("=================== end   "+new SimpleDateFormat("dd/MM/yyyy hh:mm:ss").format(new Date(System.currentTimeMillis()))+ "\n");
+//            logFileWriter.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        HtmlGrabber htmlGrabber = new HtmlGrabber(outFile);
+        htmlGrabber.grabSite("http://ita.biz.ua", "cp1251");
+
+    }
+}
