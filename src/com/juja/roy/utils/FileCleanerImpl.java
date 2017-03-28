4d6ba3755d1d4dc796914d905e0aaad25134767b
dmitriyroy <dmitriyroy@dima.Dlink>
2017-03-28 22:59:24 +0300

    Добавил интерфейс файл-клинера и базвый класс его ипмлементирующий

---
+++ src/com/juja/roy/utils/FileCleanerImpl.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.juja.roy.utils;
+
+/**
+ *
+ * @author dmitriyroy
+ */
+public class FileCleanerImpl implements FileCleaner{
+
+    @Override
+    public void CleanJavaScript() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void CleanCDATA() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void CleanComments() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
